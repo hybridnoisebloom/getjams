3ddd24163bd5a83ca22f656de64e770de8648c3c
Segmentation Fault <inhumanundead@gmail.com>
2015-01-13 16:14:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 # Released for Github on Oct 21 2014
 import musicbrainzngs as jams
 import random,sys,os,wx
-
 jams.set_useragent("GetJams","1.0","inhumanundead@gmail.com")
-
 def GetArtist(genre):
 	x = jams.search_artists(tag=genre)['artist-list']
 	y = []
@@ -21,7 +19,6 @@ def GetSimilar(artist):
 	for i in range(0,5):
 		z.append(y[random.randint(0,len(x)-1)]['sort-name'])
 	return z
-
 class GetJamsWindow(wx.Frame):
 	def __init__(self,parent,title):
 		wx.Frame.__init__(self,parent,title=title,size=(640,300))
@@ -40,7 +37,6 @@ class GetJamsWindow(wx.Frame):
 	def GenreSearch(self,event):
 		z = GetArtist(self.textbox.GetValue())
 		self.artist_text.SetLabel("Artists:\n\n"+z[0]+"\n"+z[1]+"\n"+z[2]+"\n"+z[3]+"\n"+z[4])
-
 if __name__ == "__main__":
 	app = wx.App(False)
 	frame = GetJamsWindow(None, "GetJams by Draven Stedman")
